a13c7de832cdd091d867866819cc992bf8e52921
y00tss <shepel9119@gmail.com>
2023-12-08 14:14:23 +0300

    Added GitHub Actions.

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for config project.
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
